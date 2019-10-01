bda0a4d3a98fffa2f5ead400f7acc18b97fdf35c
Rapha B <Rapha B>
2019-10-01 16:57:20 -0700

    Flipping One using W3 card flip CSS trick other cards/rows commented out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+$(document).ready(initializeApp);
 
+function initializeApp(){
+  $(".gamearea").on("click", ".card", cardClicked);
+}
+
+function cardClicked(event){
+  console.log(this, event);
+  var targetCard = $(event.currentTarget);
+  targetCard.toggleClass("flipping");
+
+}
