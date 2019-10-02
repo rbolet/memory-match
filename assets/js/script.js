da72b8cfe615364a7e1897b8c2b798172a814f29
Rapha B <Rapha B>
2019-10-01 19:41:54 -0700

    Where did the card front go?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(initializeApp);
 
+var firstCardClicked = null, secondCardClicked = null;
+var matches = null;
+
 function initializeApp(){
   $(".gamearea").on("click", ".card", cardClicked);
 }
@@ -8,5 +11,17 @@ function cardClicked(event){
   console.log(this, event);
   var targetCard = $(event.currentTarget);
   targetCard.toggleClass("flipping");
+console.log(targetCard)
+  if(firstCardClicked === null){
+    firstCardClicked = targetCard;
+  }else{
+    secondCardClicked = targetCard;
+    checkforMatch(firstCardClicked, secondCardClicked);
+  }
 
+}
+
+function checkforMatch(cardA, cardB){
+
+  console.log("Checking for match", cardA.attr("class"), cardB.attr("class"));
 }
