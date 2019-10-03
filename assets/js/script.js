a61e481a5b7b47af9da5426b0a15a91992cde1b7
Rapha B <Rapha B>
2019-10-02 17:11:33 -0700

    Basic functionality with simple 'Alert'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,68 @@
 $(document).ready(initializeApp);
 
 var firstCardClicked = null, secondCardClicked = null;
-var matches = null;
+var matches = 0;
+var maxMatches = 2;
 
 function initializeApp(){
+  clickOn()
+}
+
+function clickOn() {
   $(".gamearea").on("click", ".card", cardClicked);
 }
 
+function clickOff() {
+  $(".gamearea").unbind("click");
+}
+
 function cardClicked(event) {
-  // console.log("This", this,"event", event);
+  clickOff();
   var targetCard = $(event.currentTarget);
-console.log("targetCard", targetCard, "firstCard", firstCardClicked, "secondCard", secondCardClicked);
-  if (targetCard.is(firstCardClicked)) { console.log("Same card!");
-  return; //exit function w/o storing second card
-  }
 
-  targetCard.toggleClass("flipped"); // flip the card
+  if (targetCard.is(firstCardClicked)) {    // if the same card is clicked twice, do nothing
+    clickOn();
+    return;
+  } else{
+      targetCard.addClass("flipped");          // flip the card
 
-  if(firstCardClicked === null){ console.log("**storing first card");
-    firstCardClicked = targetCard;
-  } else{ console.log("**storing second card**");
-    //store second card, run check for match
-    secondCardClicked = targetCard;
-console.log("first card", firstCardClicked, "second card", secondCardClicked)
-    checkforMatch(firstCardClicked, secondCardClicked);
-  }
+      if(firstCardClicked === null){           // if this is the first card, store it
+        firstCardClicked = targetCard;
+        clickOn();
+        return;
+      } else {                                 //store second card, run check for match
+          secondCardClicked = targetCard;
+          setTimeout(checkforMatch,800,firstCardClicked, secondCardClicked);
+        }
+    }
 }
 
 
 function checkforMatch(cardA, cardB){
-console.log("Checking for match");
+
   var cardAClass = cardA.children().first().attr("class");
   var cardBClass = cardB.children().first().attr("class");
+  firstCardClicked = null; secondCardClicked = null;  //reset the first and second card vars
 
-    if(cardAClass == cardBClass){ console.log("Match!", "cards: ", cardA, cardB, "classes: ", cardAClass, cardBClass);
+    if(cardAClass === cardBClass){          //compare the class of the two cards
 
-      cardA.addClass("matched").removeClass("card");
+      cardA.addClass("matched").removeClass("card"); //make matched cards static
       cardB.addClass("matched").removeClass("card");
-      matches =+ 1;
-    } else{ console.log("no match");
-     // setTimeout(resetCards(cardA, cardB), 1500) ;
-    setTimeout(resetCards, 2000, cardB, cardA);
+      matches++;                            //record the match
+      checkforWin();                        // and check for a win
+    } else{
+    setTimeout(resetCards, 1000, cardB, cardA);
+      }
+}
+
+function checkforWin(){
+  if (matches === maxMatches){
+    alert("You win!");
+    return true;
+
+  } else {
+      clickOn();
+      return false;
     }
 
 }
@@ -48,8 +70,8 @@ console.log("Checking for match");
 function resetCards(){
   console.log("running Reset Cards");
   for(var iCard in arguments){
-    arguments[iCard].toggleClass("flipped");
-    firstCardClicked = null; secondCardClicked = null;
-
+    arguments[iCard].removeClass("flipped");
   }
+  firstCardClicked = null; secondCardClicked = null;
+  $(".gamearea").on("click", ".card", cardClicked);
 }
